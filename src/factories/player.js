d43263aea456442e5d4fba9aaa0462d8d2217427
guudewie <philippthiessen12@gmail.com>
2023-10-10 15:13:01 +0200

    add Player factory and test

---
+++ src/factories/player.js
@@ -0,0 +1,15 @@
+const Player = (name) => {
+
+    const attackEnemyGameboard = (enemyGameboard, coordinateArray) => {
+        let xCoord = coordinateArray[0];
+        let yCoord = coordinateArray[1];
+        enemyGameboard.receiveAttack(xCoord, yCoord)
+    }
+
+    return {
+        name,
+        attackEnemyGameboard
+    }
+}
+
+module.exports = { Player }
