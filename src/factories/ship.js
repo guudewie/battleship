99df7d688ba082cff20ca93a1832fe38c36437a1
guudewie <philippthiessen12@gmail.com>
2023-10-10 12:34:04 +0200

    add ship factory and test

---
+++ src/factories/ship.js
@@ -0,0 +1,30 @@
+const Ship = ( units ) => {
+
+    const length = units;
+
+    let hits = 0;
+
+    const getHits = () => hits
+
+    const hit = () => {
+        if (isSunk()) {
+            throw new Error("Ship already sunk")
+        } else {
+            hits++
+        }
+    }
+
+    const isSunk = () => getHits() == length ? true : false
+ 
+    return {
+        length,
+        hit,
+        hits,
+        getHits,
+        isSunk
+    }
+
+}
+
+module.exports = { Ship }
+
