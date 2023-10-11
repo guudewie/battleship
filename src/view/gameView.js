c9f0b3e1605978325ce88ed1033a9da09bd0d7e7
guudewie <philippthiessen12@gmail.com>
2023-10-11 16:39:57 +0200

    add basic dom manipulation functions

---
+++ src/view/gameView.js
@@ -0,0 +1,81 @@
+
+
+// render gameboard (gameboard.fields, visible = true)
+
+const gameView = (function () {
+
+    const gameboardLeft = document.getElementById("gameboardOne");
+    const gameboardRight = document.getElementById("gameboardTwo")
+
+    const renderGameboard = (gameboard, gameboardElement, visible = true) => {
+
+        // iterate through all fields of a gameboard
+        for (const row in gameboard.fields) {
+            for (const field in gameboard.fields[row]) {
+                
+                // create cell dom element
+                let cell = document.createElement("div")
+
+                // add field class and appropriate field state styling
+                cell.classList.add("field", hitMissShip(gameboard.fields[row][field]))
+
+                // add data row and column attribute
+                cell.dataset.row = row;
+                cell.dataset.column = field
+
+                // append field to gameboard
+                console.log(gameboardElement)
+                gameboardElement.appendChild(cell)
+            }
+        }
+    }
+
+    const clearGameboard = (gameboardElement) => {
+        // lookup first child and delete last child for performance reasons
+        while (gameboardElement.firstChild) {
+            gameboardElement.removeChild(gameboardElement.lastChild)
+        }
+    }
+
+    const setUpFieldListener = (gameboardElement, callback) => {
+        while (gameboardElement.firstChild) {
+            gameboardElement.lastChild.addEventListener("click", (e) => callback(e))
+        }
+    }
+
+    const hitMissShip = (field) => {
+
+        if (field.ship !== null && field.hit !== null) return "hitShip"  // ship that has been hit
+        if (field.ship !== null && field.hit == null) return "ship"  // ship not hit yet
+        if (field.miss) return "miss" // water but already hit: miss
+        return "water" // just water
+    }
+
+    return {
+        renderLeftGameboard (gameboard) {
+            renderGameboard(gameboard, gameboardLeft)
+        },
+
+        renderRightGameboard (gameboard) {
+            renderGameboard(gameboard, gameboardRight)
+        },
+
+        setUpFieldListenerLeft (callback) {
+            setUpFieldListener(gameboardLeft, callback)
+        },
+
+        setUpFieldListenerRight (callback) {
+            setUpFieldListener(gameboardRight, callback)
+        }
+    }
+
+})();
+
+export default gameView;
+
+// differentiate between player and computer board
+
+
+// block gameboard input
+
+// set up event listeners for gameboard
