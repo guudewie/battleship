98f7008e37f565a77f98c96a82b916bfe4674c9e
guudewie <philippthiessen12@gmail.com>
2023-10-13 11:20:48 +0200

    add computer choice and delay

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -1,10 +1,13 @@
 import gameView from "../view/gameView";
 import Gameboard from "../factories/gameboard";
 import Ship from "../factories/ship";
+import Computer from "../factories/computer";
 
 
 // this module combines dom (gameView) and logic functions (factories)
 const gameController = (function () {
+
+    const _timeOut = 700;
     
     const _makeMoveLeftPlayer = (gameboardLeft, gameboardRight) => {
         
@@ -24,30 +27,32 @@ const gameController = (function () {
             // initiate next move
             gameboardRight.getLastHit() ?
             _makeMoveLeftPlayer(gameboardLeft, gameboardRight) :
-            _makeMoveRightPlayer(gameboardLeft, gameboardRight);
+            setTimeout(() => _makeMoveRightPlayer(gameboardLeft, gameboardRight), _timeOut);
         })
     }
 
     const _makeMoveRightPlayer = (gameboardLeft, gameboardRight) => {
 
-        gameView.setUpFieldListenerLeft((field) => {
+        
+        let computerChoice = Computer.getRandomMove();
 
-            // dont set up listener if field was already played
-            if (!gameboardLeft.isValidMove(field.dataset.row, field.dataset.column)) return
+        while (!gameboardLeft.isValidMove(computerChoice[0], computerChoice[1])) {
+            computerChoice = Computer.getRandomMove();
+        }
 
-            // register attack and render updated gameboard
-            gameboardLeft.receiveAttack(field.dataset.row, field.dataset.column);
-            gameView.clearLeftGameboard();
-            gameView.renderLeftGameboard(gameboardLeft);
+        // register attack and render updated gameboard
+        gameboardLeft.receiveAttack(computerChoice[0], computerChoice[1]);
+        gameView.clearLeftGameboard();
+        gameView.renderLeftGameboard(gameboardLeft);
 
-            // return if game is over
-            if (gameboardLeft.gameboardLost() == true) return console.log("Player Left lost")
+        // return if game is over
+        if (gameboardLeft.gameboardLost() == true) return console.log("Player Left lost")
 
-            // initiate next move
-            gameboardLeft.getLastHit() ?
-            _makeMoveRightPlayer(gameboardLeft, gameboardRight) :
-            _makeMoveLeftPlayer(gameboardLeft, gameboardRight);
-        })
+        // initiate next move
+        gameboardLeft.getLastHit() ?
+        setTimeout(() => _makeMoveRightPlayer(gameboardLeft, gameboardRight), _timeOut):
+        _makeMoveLeftPlayer(gameboardLeft, gameboardRight);
+        
     }
 
     // the functions makemoveplayer recursively call each other inside another untill the win condition stops the game
--- src/factories/computer.js
+++ src/factories/computer.js
@@ -1,16 +1,21 @@
-const Computer = () => {
+const Computer = (() => {
+
+    // storge efor the current ship
 
     const getRandomMove = () => {
 
         let x = Math.floor(Math.random() * 10);
         let y = Math.floor(Math.random() * 10);
-
         return [x,y]
+    }
+
+    const getSmartMove = () => {
+
     }
 
     return {
         getRandomMove
     }
-};
+})();
 
 export default Computer;
